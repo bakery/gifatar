963ecc618b2bf2c7afe90ff5058a097c6ffc5f79
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-08-06 00:43:39 +0200

    support 2 operations: camera init + image capture

--- client/views/components/shutter-button/shutter-button.js
+++ client/views/components/shutter-button/shutter-button.js
@@ -1,21 +1,23 @@
 Template.shutterButton.events({
-    'click button' : function(){
+    'click .get-camera' : function(){
         Camera.getStream(
             function(stream){
-                // postal.publish({
-                //     topic : 'capture-image',
-                //     data : {
-                //         mode: 'manual'
-                //     }
-                // });
-
                 postal.publish({
                     topic : 'get-camera-stream'
-                });                
+                });
             },
             function(error) {
                 alert('something went terribly wrong. sorry');
             }
         );
+    },
+
+    'click .get-pic' : function(){
+        postal.publish({
+            topic : 'capture-image',
+            data : {
+                mode: 'manual'
+            }
+        });
     }
 });
