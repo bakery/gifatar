3f41415c66633036f0cd1de70014a389be2dddfd
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-08-05 23:57:39 +0200

    capture shows camera stream

--- client/views/components/capture/capture.js
+++ client/views/components/capture/capture.js
@@ -1,4 +1,19 @@
 Template.capture.created = function(){
+    
+    var template = this;
+
+    this.subGetCameraStream = postal.subscribe({
+        topic: 'get-camera-stream',
+        callback: function(){
+            Camera.getStream(
+                _.bind(Template.capture.onGotCameraStrem,template),
+                function(error) {
+                    alert('something went terribly wrong. sorry');
+                }
+            );
+        }
+    });
+
     this.subCaptureImage = postal.subscribe({
         topic: 'capture-image',
         callback: function(data, envelope) {
@@ -19,8 +34,22 @@ Template.capture.created = function(){
     });
 };
 
+Template.capture.onGotCameraStrem = function(stream){
+    var video = this.$('video')[0];
+
+    if (window.URL) {
+        video.src = window.URL.createObjectURL(stream);
+    } else {
+        video.src = stream; // Opera support.
+    }
+};
+
 Template.capture.destroyed = function(){
     if(typeof this.subCaptureImage !== 'undefined'){
         this.subCaptureImage.unsubscribe();
+    }
+
+    if(typeof this.subGetCameraStream !== 'undefined'){
+        this.subGetCameraStream.unsubscribe();
     }
 };
--- client/views/components/shutter-button/shutter-button.js
+++ client/views/components/shutter-button/shutter-button.js
@@ -2,12 +2,16 @@ Template.shutterButton.events({
     'click button' : function(){
         Camera.getStream(
             function(stream){
+                // postal.publish({
+                //     topic : 'capture-image',
+                //     data : {
+                //         mode: 'manual'
+                //     }
+                // });
+
                 postal.publish({
-                    topic : 'capture-image',
-                    data : {
-                        mode: 'manual'
-                    }
-                });
+                    topic : 'get-camera-stream'
+                });                
             },
             function(error) {
                 alert('something went terribly wrong. sorry');
