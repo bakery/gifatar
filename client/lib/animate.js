30b4eaad8dbf600df8f15976d5110874c32feced
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-08-06 01:58:39 +0200

    provide a callback for gif generation

--- client/lib/animate.js
+++ client/lib/animate.js
@@ -1,5 +1,5 @@
 Animate = {
-    images : function(imgs){
+    images : function(imgs, onComplete){
         var gif = new GIF({
                 workers: 2,
                 quality: 10
@@ -8,15 +8,12 @@ Animate = {
             imageHeight = ApplicationSettings.camera.height;
 
         gif.on('finished', function(blob) {
-            window.open(URL.createObjectURL(blob));
+            if(typeof onComplete !== 'undefined'){
+                onComplete({
+                    url : URL.createObjectURL(blob)
+                });
+            }
         });
-
-        // var bigImages = _.map(imgs, function(i){
-        //     return jQuery('img')
-        //         .attr('width', imageWidth + 'px')
-        //         .attr('height', imageHeight + 'px')
-        //         .attr('src', $(i).attr('src'))[0];
-        // });
 
         _.each(imgs, function(i){
             gif.addFrame(i);
