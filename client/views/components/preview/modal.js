4c9a0ca8eb2d5a456054cc107478254516013ff7
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-08-07 00:56:18 +0200

    implement save/discard functionality for the preview component

--- client/views/components/preview/modal.js
+++ client/views/components/preview/modal.js
@@ -4,23 +4,34 @@ Template.previewModal.created = function(){
         callback : _.bind(function(data){
             var preview = UI.renderWithData(Template.preview, data);
             this.$('.modal-body').html('');
+            
             UI.insert(preview, this.$('.modal-body')[0]);
             this.$('.modal').modal();
 
             console.log('posting', data.dataURL);
             console.log('size is', data.dataURL.length);
 
-            Meteor.call('saveGif', data.dataURL, function(error,data){
-                if(!error){
-                    alert('all good ' + data);
-                } else {
-                    alert('could not save the gif');
-                }
-            });
+            this.data.dataURL = data.dataURL;
 
         },this)
     });
 };
+
+Template.previewModal.events({
+    'click .btn-save' : function(e,template){
+
+        template.$('button').attr('disabled','disabled');
+
+
+        Meteor.call('saveGif', template.data.dataURL, function(error,gifId){
+            if(!error){
+                window.location.href = '/view/' + gifId;
+            } else {
+                alert('could not save the gif');
+            }
+        });
+    }
+});
 
 Template.previewModal.destroyed = function(){
     if(typeof this.subLaunchPreview !== 'undefined'){
